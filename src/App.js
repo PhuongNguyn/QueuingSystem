22e379309c1e6147f0cf9bb576342edb4f5d9813
zinzin.vung@gmail.com <zinzin.vung@gmail.com>
2022-06-07 05:09:11 +0700

    update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import LoginLayout from './layout/LoginLayout/LoginLayout';
 import Login from './pages/Login/Login';
 import DashBoard from './pages/Dashboard/DashBoard';
 import ForgotPassword from './pages/ForgotPassWord/ForgotPassword';
+import ResetPassword from './pages/ResetPassword/ResetPassword';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
             <Route path = "/login" element = {<LoginLayout/>}>
               <Route index element = {<Login/>}/>
               <Route path = "/login/forgotPassword" element = {<ForgotPassword/>}/>
+              <Route path = "/login/resetPassword" element = {<ResetPassword/>}/>
             </Route>
             <Route path = "/">
               <Route index element = {<DashBoard/>}/>
